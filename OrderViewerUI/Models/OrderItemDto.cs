013331630ff2e17556e866154c25af3313fd9fb3
Desire Kabwanga <dkay1101@gmail.com>
2025-06-29 16:50:00 +0200

    add OrderService to Blazor app for API integration

---
+++ OrderViewerUI/Models/OrderItemDto.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderViewerUI.Models
+{
+    public class OrderItemDto
+    {
+        public string ProductName { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
