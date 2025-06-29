62231137c782bdc71baf8e40020e5ed42408041a
Desire Kabwanga <dkay1101@gmail.com>
2025-06-29 19:39:40 +0200

    implement Blazor UI to display and manage orders

--- OrderViewerUI/Program.cs
+++ OrderViewerUI/Program.cs
@@ -1,14 +1,20 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OrderViewerUI;
+using OrderViewerUI.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 var config = builder.Configuration;
-var apiBaseUrl = config["ApiBaseUrl"];
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUrl!) });
+var apiBaseUrl = builder.Configuration["ApiBaseUrl"];
+builder.Services.AddScoped(sp =>
+    new HttpClient { BaseAddress = new Uri(apiBaseUrl!) });
+
+
+builder.Services.AddScoped<OrderService>();
+
 
 await builder.Build().RunAsync();
